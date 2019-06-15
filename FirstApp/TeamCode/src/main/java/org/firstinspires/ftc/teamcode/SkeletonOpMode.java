71109d684f412d348e6e64724cafca5bed4e5f79
Matthew Stringer <mdsflyboy@gmail.com>
2019-06-14 21:19:44 -0700

    finally functional

---
+++ FirstApp/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SkeletonOpMode.java
@@ -0,0 +1,24 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+
+public class SkeletonOpMode extends OpMode {
+
+    @Override
+    public void start() {
+    }
+
+    @Override
+    public void init() {
+
+    }
+
+    @Override
+    public void loop() {
+
+    }
+
+    @Override
+    public void stop() {
+    }
+}
