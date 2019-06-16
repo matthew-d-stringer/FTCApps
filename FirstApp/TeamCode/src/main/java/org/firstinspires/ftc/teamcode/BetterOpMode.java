806ee519cee6f19e459507a0a3cbe8da46873f1b
Matthew Stringer <mdsflyboy@gmail.com>
2019-06-15 21:07:01 -0700

    better op mode

---
+++ FirstApp/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BetterOpMode.java
@@ -0,0 +1,12 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+@TeleOp(name="BetterOpMode", group="SuperiorOpMode")
+public class BetterOpMode extends LinearOpMode {
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+    }
+}
